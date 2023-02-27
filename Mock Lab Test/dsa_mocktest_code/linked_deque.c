03802d5fd0c952261d120e9b4be91108ad2d3755
CoderVaibhavS <vaibhavsingla123.vs@gmail.com>
2023-02-28 01:16:13 +0530

    mock labtest- Q2 done

--- Mock Lab Test/dsa_mocktest_code/linked_deque.c
+++ Mock Lab Test/dsa_mocktest_code/linked_deque.c
@@ -2,6 +2,8 @@
 // REFER Corresponding header for documentation of functions. 
 linked_deque *create_linked_process_deque() {
    // COMPLETE
+   linked_deque *ld = (linked_deque *) malloc(sizeof(*ld));
+   ld->list = (process_linked_list *) malloc(sizeof(process_linked_list));
 }
 
 bool add_first_linked_deque(linked_deque *ld, process p) {
@@ -10,22 +12,27 @@ bool add_first_linked_deque(linked_deque *ld, process p) {
 
 bool add_last_linked_deque(linked_deque *ld, process p) {
     // COMPLETE
+    return add_last_to_linked_list(ld->list, p);
 }
 
 bool remove_first_linked_deque(linked_deque *ld, process *p) {
     // COMPLETE
+    return remove_first_linked_list(ld->list, p);
 }
 
 bool remove_last_linked_deque(linked_deque *ld, process *p) {
     // COMPLETE
+    return remove_last_linked_list(ld->list, p);
 }
 
 size_t get_size_linked_deque(linked_deque *ld) {
     // COMPLETE
+    return get_size_linked_list(ld->list);
 }
 
 bool is_empty_linked_deque(linked_deque *ld) {
     // COMPLETE
+    return get_size_linked_deque == 0;
 }
 
 void print_linked_deque(linked_deque *ld) {
